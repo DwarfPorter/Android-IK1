1c3be01e00c3f5210e4fc52e13cd3540f9a91dd9
DwarfPorter <mrvlmor@gmail.com>
2018-10-16 21:19:38 +0300

    Two activites run secodn activity

--- Lesson2/TwoActivities/app/src/main/java/ru/geekbrains/twoactivities/MainActivity.java
+++ Lesson2/TwoActivities/app/src/main/java/ru/geekbrains/twoactivities/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.geekbrains.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button secondActivity = findViewById(R.id.button);
+        secondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSecondActivity = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intentSecondActivity);
+            }
+        });
     }
 }
