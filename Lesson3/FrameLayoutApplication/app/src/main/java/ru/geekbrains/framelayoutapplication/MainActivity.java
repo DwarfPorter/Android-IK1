c747cabd841de6ce53839fffac980416f1fd1296
DwarfPorter <mrvlmor@gmail.com>
2018-10-21 21:52:09 +0300

    Lesson3 FrameLayout - create layout through code

--- Lesson3/FrameLayoutApplication/app/src/main/java/ru/geekbrains/framelayoutapplication/MainActivity.java
+++ Lesson3/FrameLayoutApplication/app/src/main/java/ru/geekbrains/framelayoutapplication/MainActivity.java
@@ -2,12 +2,28 @@ package ru.geekbrains.framelayoutapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        FrameLayout frameLayout = new FrameLayout(this);
+        TextView textView = new TextView(this);
+        textView.setText("Hello!");
+
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams
+                (FrameLayout.LayoutParams.WRAP_CONTENT,
+                 FrameLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL;
+
+        textView.setLayoutParams(layoutParams);
+        textView.setTextSize(42);
+        frameLayout.addView(textView);
+        setContentView(frameLayout);
     }
 }
