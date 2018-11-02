d8a7daebd04005e422341285ce6206d053870004
DwarfPorter <mrvlmor@gmail.com>
2018-11-02 22:58:51 +0300

    singltone

--- Lesson4/ChangeOrientation/app/src/main/java/ru/geekbrains/changeorientation/MainActivity.java
+++ Lesson4/ChangeOrientation/app/src/main/java/ru/geekbrains/changeorientation/MainActivity.java
@@ -8,34 +8,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int counter = 0;    // Счетчик
-    private TextView textCounter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textCounter = (TextView) findViewById(R.id.textCounter);    // Поле
-        textCounter.setText(((Integer)counter).toString());         // Выводим счетчик в поле
+        final TextView textCounter = (TextView) findViewById(R.id.textCounter);     // Текст
+        final MainPresenter presenter = MainPresenter.getInstance();      // Получить презеентер
+        textCounter.setText(((Integer)presenter.getCounter()).toString());          // Выводим счетчик в поле
         Button button = (Button) findViewById(R.id.button);         // Кнопка
         button.setOnClickListener(new View.OnClickListener() {      // Обработка нажатий
             @Override
             public void onClick(View v) {
-                counter++;
-                textCounter.setText(((Integer)counter).toString());
+                presenter.incrementCounter();   // Увеличиваем счетчик на единицу
+                textCounter.setText(((Integer)presenter.getCounter()).toString());  // Выводим счетчик в поле
             }
         });
-    }
-
-    protected void onSaveInstanceState(Bundle saveInstanceState){
-        super.onSaveInstanceState(saveInstanceState);
-        saveInstanceState.putInt("Counter", counter);               // Сохраняем счетчик
-    }
-
-    protected void onRestoreInstanceState(Bundle saveInstanceState){
-        super.onRestoreInstanceState(saveInstanceState);
-        counter = saveInstanceState.getInt("Counter");              // Востанавливаем счетчик
-        textCounter.setText(((Integer)counter).toString());         // Выводим счетчик в поле
     }
 }
