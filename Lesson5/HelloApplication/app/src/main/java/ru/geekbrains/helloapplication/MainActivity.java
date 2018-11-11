1d376ef71778c035e3587ae7480a56169dc0213d
DwarfPorter <mrvlmor@gmail.com>
2018-11-11 23:31:51 +0300

    (lesson5) отладка - лог

--- Lesson5/HelloApplication/app/src/main/java/ru/geekbrains/helloapplication/MainActivity.java
+++ Lesson5/HelloApplication/app/src/main/java/ru/geekbrains/helloapplication/MainActivity.java
@@ -2,23 +2,31 @@ package ru.geekbrains.helloapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Hello";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "onCreate: Step1");
         int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        Log.d(TAG, "onCreate: currentHour = " + currentHour );
         TextView greeting = findViewById(R.id.txtGreeting);
+        Log.d(TAG, "onCreate: greeting.getText() = " + greeting.getText());
         if (currentHour >= 12 && currentHour <= 15){
+            Log.d(TAG, "onCreate: greeting.setText(\"Good afternoon\")");
             greeting.setText("Good afternoon");
         }
         else if (currentHour >= 16 && currentHour <= 24){
+            Log.d(TAG, "onCreate: greeting.setText(\"Good evening\")");
             greeting.setText("Good evening");
         }
     }
