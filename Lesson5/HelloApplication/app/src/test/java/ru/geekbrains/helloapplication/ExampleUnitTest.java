e43c4db246f0f5b16ad99de52d93db167f1dfca6
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2018-11-13 17:05:00 +0300

    (lesson5) + tests

--- Lesson5/HelloApplication/app/src/test/java/ru/geekbrains/helloapplication/ExampleUnitTest.java
+++ Lesson5/HelloApplication/app/src/test/java/ru/geekbrains/helloapplication/ExampleUnitTest.java
@@ -12,11 +12,35 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
 
     @Test
-    public void Greeting_get_isEvening(){
+    public void Greeting_get_isEvening16() {
         Greeting greeting = new Greeting(16);
         String actual = greeting.get();
         String expected = "Good evening";
         assertEquals(expected, actual);
-
     }
+
+    @Test
+    public void Greeting_get_isEvening24() {
+        Greeting greeting = new Greeting(24);
+        String actual = greeting.get();
+        String expected = "Good evening";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Greeting_get_isAfternoon12(){
+        Greeting greeting = new Greeting(12);
+        String actual = greeting.get();
+        String expected = "Good afternoon";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Greeting_get_isAfternoon15(){
+        Greeting greeting = new Greeting(15);
+        String actual = greeting.get();
+        String expected = "Good afternoon";
+        assertEquals(expected, actual);
+    }
+
 }
