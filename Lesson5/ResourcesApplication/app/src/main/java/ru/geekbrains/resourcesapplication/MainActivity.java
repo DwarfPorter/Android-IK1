b6f76f9449754340bbdeb8515ab7a832618bfcb0
DwarfPorter <mrvlmor@gmail.com>
2018-11-16 22:54:20 +0300

    (lesson5) ресурсы

---
+++ Lesson5/ResourcesApplication/app/src/main/java/ru/geekbrains/resourcesapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.resourcesapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str = getResources().getString(R.string.HelloWorld);
+
+    }
+}
