db039b0757c471392670e7223d4be9dbc9d75ba0
DwarfPorter <mrvlmor@gmail.com>
2018-11-19 23:24:59 +0300

    (lesson6)(cityinfo) сделали список в фрагменте

--- Lesson6/CityInfo/app/src/main/java/ru/geekbrains/cityinfo/CitiesFragment.java
+++ Lesson6/CityInfo/app/src/main/java/ru/geekbrains/cityinfo/CitiesFragment.java
@@ -3,10 +3,12 @@ package ru.geekbrains.cityinfo;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 
 /**
@@ -27,6 +29,16 @@ public class CitiesFragment extends ListFragment {
         return inflater.inflate(R.layout.fragment_cities, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-    
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.Cities,
+                android.R.layout.simple_list_item_activated_1);
+
+        setListAdapter(adapter);
+
+
+    }
 }
