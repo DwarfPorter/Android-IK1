03759533bb5f93a6b6b1d4fa641870d1e9614bd8
DwarfPorter <mrvlmor@gmail.com>
2018-11-20 23:54:59 +0300

    (lesson6)(cityinfo) Модернизировали фрагмент для работы со стеком

--- Lesson6/CityInfo/app/src/main/java/ru/geekbrains/cityinfo/CoatofarmsFragment.java
+++ Lesson6/CityInfo/app/src/main/java/ru/geekbrains/cityinfo/CoatofarmsFragment.java
@@ -3,11 +3,13 @@ package ru.geekbrains.cityinfo;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,14 +43,23 @@ public class CoatofarmsFragment extends Fragment implements Constants {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-// определить какой герб надо показать, и показать его
-        ImageView coatOfArms = new ImageView(getActivity());
+        View layout = inflater.inflate(R.layout.fragment_coatofarms, container, false);
+        return layout;
+    }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // определить какой герб надо показать, и показать его
+        ImageView coatOfArms = getActivity().findViewById(R.id.imageView);
+        if (coatOfArms == null) return;
         // получить из ресурсов массив указателей на изображения гербов
         TypedArray imgs = getResources().obtainTypedArray(R.array.coatofarms_imgs);
         // выбрать по индексу подходящий
         coatOfArms.setImageResource(imgs.getResourceId(index, -1));
-        return coatOfArms; // Вместо макета используем сразу картинку
+        TextView city = getActivity().findViewById(R.id.textView);
+        String[] names = getResources().getStringArray(R.array.Cities);
+        city.setText(names[index]);
     }
 
 }
