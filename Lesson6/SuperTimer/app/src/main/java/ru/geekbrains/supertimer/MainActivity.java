2e74475acb6342c37849cae5572824d8606e068d
vladimir.morozov <vladimir.morozov@totalobjects.spb.ru>
2018-11-26 09:46:48 +0300

    установка фрагмента

--- Lesson6/SuperTimer/app/src/main/java/ru/geekbrains/supertimer/MainActivity.java
+++ Lesson6/SuperTimer/app/src/main/java/ru/geekbrains/supertimer/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.supertimer;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainTimerFragment mainFragment = new MainTimerFragment();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_main, mainFragment);
+        fragmentTransaction.commit();
     }
 }
